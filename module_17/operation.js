d6236dda66e8c707300e91cb1aa9e4acdd3fa682
Aminul667 <rahathssn@gmail.com>
2023-01-25 21:58:42 +0600

    added 17_8

--- module_17/operation.js
+++ module_17/operation.js
@@ -12,3 +12,20 @@ console.log("subtraction: ", subtraction);
 console.log("division: ", division);
 console.log("multiplication: ", multiplication);
 console.log("modular: ", modular);
+
+addition += 1;
+subtraction -= 1;
+division /= 1;
+multiplication *= 1;
+
+console.log("after: ")
+console.log("addition: ", addition);
+console.log("subtraction: ", subtraction);
+console.log("division: ", division);
+console.log("multiplication: ", multiplication);
+
+var firstName = "Hello"
+var lastName = "World"
+var fullName = firstName + " " + lastName
+
+console.log(fullName)
---
+++ module_17/typecasting.js
@@ -0,0 +1,9 @@
+var price1 = 10;
+var price2 = "31";
+
+var tPrice = price1 + price2;
+
+console.log(tPrice);
+
+// convert string to number
+console.log(price1 + parseInt(price2));
