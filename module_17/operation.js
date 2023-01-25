0eff770f2929958f5338feb936c6a9ab736fa97e
Aminul667 <rahathssn@gmail.com>
2023-01-25 21:16:11 +0600

    added 17_5 and 17_6

---
+++ module_17/operation.js
@@ -0,0 +1,14 @@
+var a = 10;
+var b = 20.5;
+
+var addition = a + b;
+var subtraction = b - a;
+var division = b / a;
+var multiplication = a * b;
+var modular = b % 2;
+
+console.log("addition: ", addition);
+console.log("subtraction: ", subtraction);
+console.log("division: ", division);
+console.log("multiplication: ", multiplication);
+console.log("modular: ", modular);
---
+++ module_17/variable_name.js
@@ -0,0 +1,7 @@
+var first_name = "Arnold Floyd"; // snake case  
+var firstName = "Arnold Floyd";  // camel case
+var FirstName = "Arnold Floyd"   // pascal case
+
+console.log(first_name)
+console.log(firstName)
+console.log(FirstName)
