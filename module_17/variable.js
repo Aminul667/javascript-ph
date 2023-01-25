3ce17b952791aab744d1c0105ca44606eba0511d
Aminul667 <rahathssn@gmail.com>
2023-01-25 20:51:30 +0600

    added 17_4

---
+++ module_17/variable.js
@@ -0,0 +1,16 @@
+// numeric
+var age = 26;
+
+// string
+var name = "RK";
+var occupation = "Job";
+
+// boolean
+var boolT = true;
+var boolF = false;
+
+console.log(age, name, occupation);
+console.log(name);
+console.log(occupation);
+
+console.log(boolT, boolF)
