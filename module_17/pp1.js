03f69b9307b8dfdd9debc9772fe9477575582a27
Aminul667 <rahathssn@gmail.com>
2023-01-26 10:36:26 +0600

    added practice problems

---
+++ module_17/pp1.js
@@ -0,0 +1,6 @@
+var totalMoney = 1000;
+var totalCost = 700;
+
+var change = totalMoney - totalCost;
+
+console.log(change);
---
+++ module_17/pp4.js
@@ -0,0 +1,5 @@
+var num = 119;
+
+var remainder = num % 5;
+
+console.log(remainder);
