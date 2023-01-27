6c7e3ea4601d9d340c81e620cb740fd51edba53b
Aminul667 <rahathssn@gmail.com>
2023-01-27 19:17:03 +0600

    added 18_2 array and conditional statement

---
+++ module_18/array.js
@@ -0,0 +1,23 @@
+var numbers = [10, 20, 30, 40, 50];
+var names = ["Hello", "world", "Hi"]
+
+console.log(numbers)
+console.log(names)
+
+console.log(numbers[3])
+console.log(names[2])
+
+console.log(numbers.length)
+console.log(names.length)
+
+console.log(names.indexOf("world"))
+console.log(names.indexOf("RK"))
+
+numbers.push(100);
+names.push("Hola")
+
+console.log(numbers)
+console.log(names)
+
+names.pop()
+console.log(names)
---
+++ module_18/conditional.js
@@ -0,0 +1,9 @@
+var price = 5000;
+var number = 4000;
+
+if (price < number) {
+  console.log(price);
+} 
+else {
+  console.log(number);
+}
